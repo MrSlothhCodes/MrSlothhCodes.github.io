b4ac362ad26002b316a6b4c2deeb55233be14d5f
Suryansh Ahuja <119343528+MrSlothCodes@users.noreply.github.com>
2023-02-07 14:24:13 +0530

    audio and backspace code

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-*{
-    font-size: 34px;
-    color: rgb(254, 250, 250);
-    text-align: center;
-    font-family:'Courier New', Courier, monospace;;
-    
-}
-input {
-    background-color: rgb(79, 79, 96);
-    width: 65vw;
-    max-width: 225px;
-    text-align: right;
-    
-}
-button {    
-    background-color: rgb(79, 79, 96);
-    width: 20vw;
-    height: 10vh;   
-    max-width: 75px;
-    max-height: 50px;
-}
-.bt2{
-    background-color: rgb(79, 79, 96);
-    width: 13vw;
-    height: 10vh;   
-    max-width: 60px;
-    max-height: 45px;
-    margin-left: 5px;
-}
-.calc {  
-    padding: 10px;
-    display: inline-block;
-    background-color: rgb(132, 132, 153);
-   
-}
-a,h1 {
-    color: black;
-}
-table {
-    display: block;
-    margin: 10px;
+let screen = document.getElementById('screen');
+let screenValue = ' ';
+btn = document.querySelectorAll('button');
+
+const audio = new Audio ("Assets/button.ogg")
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
+
+for (item of btn) {
+    item.addEventListener('click', (e) => {
+        btntext = e.target.innerText;
+
+        if (btntext == 'X') {
+            btntext = "*";
+            screenValue += btntext
+            screen.value = screenValue;
+        }
+        else if (btntext == 'C') {
+            screenValue = " ";
+            screen.value = screenValue;
+        }
+        else if (btntext == '←') {
+            let str = screenValue.toString();
+            screenValue = str.substring(0, str.length - 1);
+            
+            screen.value = screenValue;
+        }
+        else if (btntext == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += btntext
+            screen.value = screenValue;
+        }
+    })
 }
